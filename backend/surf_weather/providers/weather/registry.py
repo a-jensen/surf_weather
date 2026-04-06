1f72c68435cca925a50e37d0a7e4e8e5336c8fd5
Austin Jensen <austin.m.jensen@gmail.com>
2026-04-06 00:03:30 -0600

    Untrack all.

--- backend/surf_weather/providers/weather/registry.py
+++
@@ -1,7 +0,0 @@
-from ..base import WeatherProvider
-from .open_meteo import OpenMeteoProvider
-
-
-def get_weather_provider() -> WeatherProvider:
-    """Return the configured weather provider (Open-Meteo by default)."""
-    return OpenMeteoProvider()
